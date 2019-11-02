f747da8c3304cf06e25c3b06491c720983e573b5
Prashant Goyal <myidispg@gmail.com>
2019-11-02 19:41:43 +0530

    Added text display too

--- pytorch_implementation/detector.py
+++ pytorch_implementation/detector.py
@@ -40,14 +40,11 @@ if CUDA:
 model.eval()
 
 # Read the images
-# img = cv2.imread('dog-cycle-car.png')
-img = cv2.imread('Screenshot (2).png')
+img = cv2.imread('dog-cycle-car.png')
+# img = cv2.imread('WIN_20191102_19_32_43_Pro.jpg')
 orig_shape = img.shape
 scaling_factor = [orig_shape[0]/inp_dim, orig_shape[1]/inp_dim]
-# scaling_factor = [inp_dim/orig_shape[0], inp_dim/orig_shape[1]]
-# scaling_factor = np.array([inp_dim/orig_shape[0], inp_dim/orig_shape[1]]).min()
-print(orig_shape)
-print(scaling_factor)
+
 # Preprocess the image
 img_tensor= prep_image(img, inp_dim)
 # Convert to PyTorch tensor
@@ -70,10 +67,7 @@ for single_prediction in prediction:
     # Resize the coordinates according to the original image
     single_prediction[[1, 3]] *= scaling_factor[1]
     single_prediction[[2, 4]] *= scaling_factor[0]
-    # single_prediction[[1, 3]] -= (inp_dim - scaling_factor[0]*orig_shape[0])/2
-    # single_prediction[[2, 4]] -= (inp_dim - scaling_factor[1]*orig_shape[1])/2
-    # single_prediction[[1, 3]] /= scaling_factor[0]
-    # single_prediction[[2, 4]] /= scaling_factor[1]
+
     # Clip any bounding boxes with dimensions outside our image.
     single_prediction[[1, 3]] = np.clip(single_prediction[[1, 3]], 0.0, orig_shape[0])
     single_prediction[[2, 4]] = np.clip(single_prediction[[2, 4]], 0.0, orig_shape[1])
@@ -82,4 +76,9 @@ for single_prediction in prediction:
     c1 = (int(single_prediction[1]), int(single_prediction[2]))
     c2 = (int(single_prediction[3]), int(single_prediction[4]))
     cv2.rectangle(img, c1, c2, (255, 0, 0), 1)
+
+    # Write the class label
+    text_size = cv2.getTextSize(class_label, cv2.FONT_HERSHEY_PLAIN, 1, 1)[0]
+    cv2.putText(img, class_label, (c1[0], c1[1] + text_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [255, 0, 0], 1)
+
 cv2.imwrite('image.png', img)
